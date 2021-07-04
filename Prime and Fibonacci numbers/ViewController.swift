4743ce61ac7357a05a898b16c365d7f290c9e75b
sergunya95 <sergunya95@gmail.com>
2021-07-04 23:52:15 +0300

    Initial Commit

---
+++ Prime and Fibonacci numbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prime and Fibonacci numbers
+//
+//  Created by apple on 7/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
